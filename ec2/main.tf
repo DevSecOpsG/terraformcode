2dd6e363cfd63fa3a25b4cacd43fd0c9e485dac0
DevOpsG <76554480+DevSecOpsG@users.noreply.github.com>
2022-03-23 17:54:47 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-0e28512303d860de7"
   instance_type = "t2.micro"
 
   tags = {
