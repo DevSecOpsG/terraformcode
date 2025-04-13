dce7084edacb7ea0d420d3d9f28296f669d3fce1
rakesh-mane <rakeshmane981@gmail.com>
2025-04-13 16:45:09 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-064ff912f78e3e561"
+  ami           = "ami-0c02fb55956c7d316"
   instance_type = "t2.micro"
 
   tags = {
